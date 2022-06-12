69afd8e3cc73eb03d9260eeb1a330f01a6fa4cad
Ranjith <2k18cse082@kiot.ac.in>
2022-06-12 21:00:14 +0530

    Project Template with team Repo

---
+++ BaseballMVC.UI/Repositories/TeamsRepository.cs
@@ -0,0 +1,46 @@
+﻿
+using BaseballMVC.DataService;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BaseballMVC.UI.Repositories
+{
+    public class TeamsRepository
+    {
+        public BaseballEntities dbEntities ;
+        public TeamsRepository()
+        {
+            dbEntities = new BaseballEntities();
+        }
+        public void AddTeams(Team team)
+        {
+            dbEntities.Teams.Add(team);
+            dbEntities.SaveChanges();
+        }
+        public void EditTeam(Team team)
+        {
+            dbEntities.Entry(team).State = EntityState.Modified;
+            dbEntities.SaveChanges();
+
+        }
+        public void DeleteTeam(Team team)
+        {
+            dbEntities.Teams.Remove(team);
+            dbEntities.SaveChanges();
+        }
+       /* public IEnumerable<Team> GetTeams(int id)
+        {
+            var team = dbEntities.Teams.Where(teams => teams.teamId == id);
+            return team;
+        }*/
+        public Team GetReview(int id)
+        {
+            var planReviews = dbEntities.Teams.Where(teams => teams.teamId == id).FirstOrDefault();
+            return team;
+        }
+
+    }
+}
